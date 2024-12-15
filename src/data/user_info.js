d0cd4686f25cc36549acc6269fe01e2c693c6b0f
Jaivik Tailor <jaiviktailor@Jaiviks-MacBook-Pro.local>
2024-12-15 18:23:42 -0500

    Added Portfolio

---
+++ src/data/user_info.js
@@ -0,0 +1,163 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Jaivik Tailor",
+        description: "I am a dedicated software engineer with expertise in full-stack development, driven by a passion for solving complex challenges. I focus on delivering high-quality, efficient solutions while continuously enhancing my skills. With extensive experience in diverse programming languages, frameworks, and cloud technologies, I thrive in fast-paced, agile environments where innovation and collaboration are key.",
+        role: "Full-Stack Developer",
+        photo: "pp1.png",
+        email: "jaivik.tailor@dal.ca",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+		github: "https://github.com/Jaivik3438",
+		linkedin: "https://www.linkedin.com/in/jaiviktailor/",
+		instagram: "https://www.instagram.com/jaiviktailor/",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+            title: "Serverless Trivia Game",
+            description: "Developed a serverless trivia game compatible with multiple cloud platforms, primarily AWS and GCP, featuring real-time gameplay, dynamic leaderboards, and seamless integration for enhanced scalability and performance.",
+            technologies: "AWS, GCP, JavaScript, Python",
+            github: "https://github.com/Jaivik3438/-Serverless-Trivia-Game.git",
+            link: "https://github.com/Jaivik3438/-Serverless-Trivia-Game/blob/main/Cloud%20Architecture.drawio.png"
+        },
+        {
+			title: "Email Marketing System",
+			description: "Developed a personalized campaign-generation platform that enables the creation and delivery of customized email campaigns to subscribers, integrating user data for targeted communication and enhanced engagement.",
+            technologies: "Java, Spring, JavaScript, Node",
+			github: "https://github.com/Jaivik3438/Email-Marketing-System.git",
+            link: ""
+		},
+
+        {
+			title: "TO-DO-LIST Maker",
+			description: "Developed a simple To-Do List Maker with full CRUD operations, utilizing various AWS services to manage application flow, ensure data persistence, and enhance performance and scalability.",
+			technologies: "AWS-SDK, DynamoDB, AWS SNS, Lambda, Node js",
+			github: "https://github.com/Jaivik3438/ToDo-List-Maker.git",
+            link: "https://github.com/Jaivik3438/ToDo-List-Maker/blob/main/Inrastructure._Final.png"
+		},
+
+        {
+			title: "Pet Shield",
+			description: "Developed a feature-rich MERN stack application for Pet Care Management, enabling efficient scheduling, tracking, and management of pet care activities with a user-friendly interface and robust backend architecture.",
+			technologies: "React.js, Express.js, MongoDB, Node js, Figma",
+			github: "https://github.com/Jaivik3438/PetShield.git",
+            link: "https://spontaneous-blancmange-9e6574.netlify.app"
+		},
+
+		{
+			title: "TO_DO_LIST-AWS-Optimization",
+			description: "Analyzed an existing application to identify enhancement opportunities and implemented improvements using AWS services, focusing on optimizing performance, scalability, and reliability while leveraging cloud-based solutions.",
+			technologies: "AWS Services, Analysis, Optimization",
+			github: "https://github.com/Jaivik3438/TO_DO_LIST-AWS-Optimization",
+            link: "https://github.com/Jaivik3438/TO_DO_LIST-AWS-Optimization/blob/main/Jaiviik_Tailor_AWS_Optimization_report.pdf"
+		},
+
+		{
+			title: "Insurance Intelligence",
+			description: "Developed a Spring Boot application that predicts flight delays using machine learning algorithms applied to a Kaggle dataset, incorporating data analysis and visualization to provide insights and improve prediction accuracy.",
+			technologies: "JAVA, Spring Boot, Machine Learning",
+			github: "https://github.com/sanketpatel98/SpringBoot.git",
+            link: ""
+		},
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Dalhosuie University",
+            degree: "Master in Applied Computer Science | GPA: 3.83/4.3",
+            duration: "2022 - 2023",
+            image: "https://images.squarespace-cdn.com/content/v1/5490a87ce4b01fef2753a84e/1727208126839-T3Z8R9FWNJ0FHNRYDUUF/Screenshot+2024-09-24+at+5.01.29+PM.png"
+        },
+        {
+            school: "Gujarat Technological University",
+            degree: "Bachelors in Computer Engineering | GPA: 3.75/4.0",
+            duration: "2016 - 2020",
+            image: "https://upload.wikimedia.org/wikipedia/en/9/93/Gujarat-Technological-University-Logo.png"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "IT Developer/Analyst",
+            company: "Canada Revenue Agency",
+            duration: "Sep 2023 - Current",
+            image: "https://www.krp.ca/wp-content/uploads/2024/02/CRA-Banner.jpg",
+            descriptions: [
+                "Crafting a robust web app with Angular frontend, Java backend, and integrated multiple APIs for seamless data transfer.",
+                "Utilizing Agile methodologies & Jira for efficient project deliveries, including sprint planning and task allocation.",
+                "Managing version control with Git for collaborative code development on the web app's codebase.",
+                "Integrating Angular and Java systems for improved user experience and system reliability that enhanced 50% of performance.",
+                "Aligning project goals with client requirements, ensuring on-time delivery and boosting project effectiveness by 60%.",
+                "delivering cost-effective & efficient public facing applications for Canadian residers",
+            ]
+        },
+        {
+            position: "Software Developer ",
+            company: "Softvan Pvt. Ltd.",
+            duration: "Mar 2021 - Sep 2021",
+            image: "https://i.ibb.co/Ns06KrS/1630569343300.jpg",
+            descriptions: [
+                "Programmed Backend RESTful API for Sports E-Commerce Store using Nodejs and ExpressJS MVC Architecture.",
+                "Proposed & designed NoSQL database structure using MongoDB to manage admin, user, & product data.",
+                "Collaborated with other interns and software developers to manage the spring backlogs.",
+                "Assisted senior developers by accurately debugging and fixing bugs before deadline.",
+                "Designed 20+ User Interface and User Experience dashboards with Usability Principles.",
+                "Integrated GitHub into a robust CI/CD pipeline, fostering a cohesive development workflow for Angular and Java (SpringBoot).",
+            ]
+        },
+        {
+            position: "Junior Software Developer",
+            company: "KiwiMiles India",
+            duration: "Sept 2020 - Feb 2021",
+            image: "https://i.ibb.co/rfRpbZs/kiwimiles.jpg",
+            descriptions: [
+                "Conducted client consultation & requirement validation for an E-Commerce Store, ensuring comprehension of client objectives.",
+                "Led the development of a sophisticated web application for international client, utilizing Angular 8 for frontend and NestJS for backend",
+                "Employed Docker to manage a PostgreSQL database, ensuring a scalable and containerized environment for the application.",
+                "Implemented robust security using NestJS/JWT and integrated NestJS/PDF for enhanced document management.",
+                "Optimized project with AWS Elastic Beanstalk & Amazon S3 for improved application performance and data management.",
+                "Collaborated in Agile setting to manage sprints and capture user requirements effectively with stakeholders."
+            ]
+        },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Git Essential Training",
+            description: "Provided by LinkedIn Learning",
+            icon: "linkedinlearning",
+            link: "https://www.linkedin.com/learning/certificates/947f8fcd35d7ff9c4f43b48b3f4b1078ea96caf08f0a18117de52f848798c47b",
+        },
+        {
+            title: "MongoDB",
+            description: "Provided by LinkedIn Learning",
+            icon: "linkedinlearning",
+            link: "https://www.linkedin.com/learning/certificates/473a524df36067bae2536996502a937537d63725c42109ea90e6e50817e1c854",
+        },
+        {
+            title: "International Experience Program",
+            description: "Provided by Launrentian/Université Laurentienne And GTU",
+            icon: "study",
+            link:"https://www.linkedin.com/in/jaiviktailor/details/certifications/",
+        },
+      
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for visiting my website! I truly value your feedback, questions, and suggestions. If you’d like to reach out, feel free to email me directly at jaivik.tailor@dal.ca. I strive to respond to all messages within 24 hours, though response times may vary during busy periods. You can also connect with me on social media through the links below. I look forward to hearing from you!",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Jaivik Tailor. All Rights Reserved"
+}
+
+export default info
